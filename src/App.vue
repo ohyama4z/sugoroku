99c285df77151643a77d21be5b23039456018e55
Ohyama <takumi0616zz@gmail.com>
2018-12-23 17:01:06 +0900

    コマの動きまで

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
-    <Dice v-on:rolled="diceRolled"></Dice>
+    <h1 v-if="position === goal">ゴーーーーール！！！！！！！！！</h1>
+    <Dice
+      v-else
+      v-on:rolled="rolledDice"
+    ></Dice>
+    <Sheet 
+      v-bind:goal="goal"
+      v-bind:position="position"
+    ></Sheet>
   </div>
 </template>
 
 <script>
 import Dice from './components/Dice.vue'
+import Sheet from './components/Sheet.vue'
 
 export default {
   name: 'app',
   components: {
-    Dice
+    Dice,
+    Sheet
+  },
+  data () {
+    return {
+      goal: 12,
+      position: 1
+    }
   },
   methods: {
-    diceRolled (num) {
-      console.log(num)
+    rolledDice (num) {
+      if (this.position + num > this.goal) {
+        this.position = this.goal
+      } else {
+        this.position += num
+      }
     }
   }
 }
---
+++ src/components/Sheet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div
+      class="square"
+      v-for="i in goal"
+      v-bind:key="i"
+    >
+      {{ i }}<br>
+      <div v-if="i === position">あんパン</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sheet',
+  props: [
+    'position',
+    'goal'
+  ],
+  components: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+.square {
+  width: 100px;
+  height: 100px;
+  border: solid 1px #000000;
+}
+</style>
